66f6e81935f6f447d615360d3648ab74e6ab84f9
Ralph Schuler <48420064+ralphschuler@users.noreply.github.com>
2025-12-11 01:48:23 +0100

    Log payload after flushing metrics

--- packages/screeps-graphite-exporter/src/consoleListener.ts
+++ packages/screeps-graphite-exporter/src/consoleListener.ts
@@ -153,6 +153,7 @@ export async function startConsoleListener(api: ScreepsAPI, metrics: Metrics, lo
     if (lines.length > 0) {
       metrics.flush();
     }
+    console.log(payload)
   });
 
   if (typeof socket.connect === 'function') {
